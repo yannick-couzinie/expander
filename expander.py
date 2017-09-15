d2eb8ca752249b35642c76d0546204d27ab88b5e
Yannick Couzinié <yannick.couzinie@cognostics.de>
2017-09-15 10:17:27 +0200

    Move expander related code to subdirectory
    The expander related code has been moved to a subdirectory, so as to be
    able to use it as a submodule. Since the task it is supposed to do can
    be done very isolated, I deemed it more useful to have it as a module
    rather than cluttering the utils file or main directory. Since I do
    expect this to become quite cumbersome do get correctly.

---
+++ expander.py
@@ -0,0 +1,161 @@
+"""
+Class for expanding contractions in english text.
+
+TODO-list:
+    - Fix the case for Who'd've thought, in which Who'd gets recognized
+      as only one word while it is two. Causing the program to replace 3
+      words.
+    - Disambiguate replacements where the list has multiple elements!
+    - Is it possible to replace only using the contraction part and not
+      the part in front of that? In an attempt to handle names.
+    - Include a test case when this is run as __main__, correctly
+      asserting that the right results come out.
+
+"""
+
+# standard library imports
+import yaml
+# third party library imports
+import nltk
+# local imports
+import utils
+
+
+def expand_contractions(stanford_model, sent_list, is_split=True):
+    """
+    This method uses the StanfordPOSTagger tags to identify contractions in
+    the sentence and expand them sensibly. Some examples are:
+        "I'm" -> "I am"
+        "It's difficult" -> "It is difficult"
+    The difficulty is that sometimes "'s" is not an indicator of a
+    contraction but a possessive pronoun like
+        "It's legs were shaking"
+    which should not be expanded. The stanford tagger tags this as
+    "POSS" for possessive which makes it easy to identify these cases.
+    Furthermore, a difficulty lies in the fact that the expansion is not
+    unique. Without context we have for example the following:
+        "I'll" -> "I will" or "I shall"
+
+    Args:
+        stanford_model - object of StanfordPOSTagger, as returned by
+                         load_stanford_pos.
+        sent_list - list of sentences which are split up by word.
+                    For the splitting use nltk.word_tokenize.
+        is_split - boolean to track whether splitting has to be done
+                   or not. If it has to be done provide sentences as
+                   single strings.
+
+    Returns:
+        sent_list with expanded contractions.
+    """
+    tuple_list = _conv_2_word_pos(stanford_model, sent_list, is_split)
+
+    with open("contractions.yaml", "r") as stream:
+        contractions = yaml.load(stream)
+
+    for i, sent in enumerate(tuple_list):
+        # look at all the sentences in the list
+        for j, word_pos_tuple in enumerate(sent):
+            word = word_pos_tuple[0]
+            pos = word_pos_tuple[1]
+            # check that it is a contraction
+            # an example of this would be "'m"
+            if word[0] == "'":
+                # if it is a contraction we need to combine all the
+                # contractions. Since our dictionary knows "I'm" and not
+                # "'m".
+                # Else just do nothing.
+                if sent[j+1][0][0] == "'":
+                    # if the next word is also a contraction combine all
+                    # three, three is the maximum possible.
+                    word = ''.join([sent[j-1][0], sent[j][0],
+                                    sent[j+1][0]])
+                else:
+                    # otherwise join only two words
+                    word = ''.join([sent[j-1][0], sent[j][0]])
+
+                if pos != 'POS':  # for possessive
+                    if word.lower() in contractions:
+                        # split the string to be inserted up
+                        replace = contractions[word.lower()][0]
+                        replace = nltk.word_tokenize(replace)
+                        # we need to check whether the first letter is
+                        # upper case for the replacement.
+                        if word[0].isupper():
+                            replace[0] = replace[0].capitalize()
+                    elif word in contractions:
+                        # split the string to be inserted up
+                        replace = contractions[word][0]
+                        replace = nltk.word_tokenize(replace)
+                    else:
+                        replace = None
+
+                    # replace the word part in the word_pos_tuples with
+                    # the expanded word.
+                    if len(replace) == 2:
+                        tuple_list[i][j-1] = (replace[0],
+                                              tuple_list[i][j-1][1])
+                        tuple_list[i][j] = (replace[1],
+                                            tuple_list[i][j][1])
+
+                    elif len(replace) == 3:
+                        tuple_list[i][j-1] = (replace[0],
+                                              tuple_list[i][j-1][1])
+                        tuple_list[i][j] = (replace[1],
+                                            tuple_list[i][j][1])
+                        tuple_list[i][j+1] = (replace[2],
+                                              tuple_list[i][j+1][1])
+                    elif replace is None:
+                        pass
+                    else:
+                        raise RuntimeError("This should never happen.")
+    return tuple_list
+
+
+def _conv_2_word_pos(stanford_model, sent_list, is_split):
+    """
+    Converts a sentence list to a list of lists, where the lists contain
+    (word, pos_tag) tuples using the provided stanford_model.
+
+    Args:
+        stanford_model - object of StanfordPOSTagger, as returned by
+                         load_stanford_pos.
+        sent_list - List with sentences split up into list of singular words
+                    e.g. [["I", "am", "sentence", "one"],
+                          ["I", "am", "sentence", "two"]]
+        is_split - if False then the input should be
+                         ["I am sentence one",
+                          "I am sentence two"]
+
+    Returns:
+           output - the same list of sentences, with each word replaced by
+                     a (word, tag) tuple.
+    """
+    output = []
+    if not is_split:
+        for sent in sent_list:
+            output.append(nltk.word_tokenize(sent))
+    else:
+        output = sent_list
+    output = [stanford_model.tag(e) for e in output]
+    return output
+
+
+if __name__ == '__main__':
+    TEST_CASES = [
+        "I'm a bad person",  # 'm -> am
+        "It's not what you think",  # 's -> is
+        "It's his cat anyway",  # 's -> is
+        "It's a man's world",  # 's -> is and 's possessive
+        "Catherine's been thinking about it",  # 's -> has
+        "It'll be done",  # 'll -> will
+        "Who'd've thought!",  # 'd -> would, 've -> have
+        "She said she'd go.",  # she'd -> she would
+        "She said she'd gone.",  # she'd -> had
+        ]
+    # use nltk to split the strings into words
+    MODEL = utils.load_stanford_pos()
+    # get the list oif pos_tags
+    EXPANDED_LIST = expand_contractions(MODEL, TEST_CASES, is_split=False)
+    for SENT in EXPANDED_LIST:
+        print(SENT)
---
+++ utils.py
@@ -0,0 +1,67 @@
+""" General utilities file """
+
+# standard library imports
+import glob
+import os
+# third party library imports
+import nltk
+
+
+def load_stanford_pos(dirname=None,
+                      model_name="english-bidirectional-distsim.tagger"):
+    """
+    Load the Stanford POS-tagging module. For this you have to download
+    the model from:
+
+        https://nlp.stanford.edu/software/stanford-postagger-2017-06-09.zip
+
+    and make sure the file english-bidirectional-distsim.tagger is in
+    the directory under dirname.
+    Args:
+        dirname - Directory name where the model is located, if none is
+                  supplied it is assumed to be in ./stanford_models.
+        model_name - Name of the model to be used, if none is supplied,
+                     the standard stanford tagger is used.
+    Returns:
+        An object of StanfordPOSTagger, which can be used like
+            st.tag('What is the airspeed of an unladen swallow ?'.split())
+        to obtain an output
+        [('What', 'WP'), ('is', 'VBZ'), ('the', 'DT'), ('airspeed', 'NN'),
+         ('of', 'IN'), ('an', 'DT'), ('unladen', 'JJ'), ('swallow', 'VB'),
+         ('?', '.')]
+    Raises:
+        LookupError if the model is not found.
+    """
+    if dirname is None:
+        # set model dir to ./asset/models
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        model_dir = os.path.join(current_dir, "stanford_models")
+    # glob for the class recursively in model_dir
+    glob_dir = os.path.join(model_dir,
+                            "**",
+                            "stanford-postagger.jar")
+    classes = glob.glob(glob_dir,
+                        recursive=True)
+
+    if len(classes) > 1:
+        # only have one stanford postagger version at any time
+        raise LookupError("Multiple PosTaggers found, please only have"
+                          " one version downloaded at any time.")
+    # point model_dir to the models directory in the stanford pos
+    # directory
+    model_dir = os.path.dirname(classes[0])
+    model_dir = os.path.join(model_dir, "models")
+
+    if not os.path.exists(model_dir):
+        raise LookupError("The directory /asset/models/ does not"
+                          " exist, make sure you create it and place"
+                          " file english-bidirectional-distsim.tagger"
+                          " in it. Alternatively, supply the directory"
+                          " to loadStanfordPos() in which it is located.")
+    # set the environment variables so that nltk can find the models
+    os.environ["CLASSPATH"] = classes[0]
+    os.environ["STANFORD_MODELS"] = model_dir
+
+    # load the model
+    stanford_model = nltk.tag.StanfordPOSTagger(model_name)
+    return stanford_model

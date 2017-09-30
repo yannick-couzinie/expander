4013ed7cae95d375422c4998f8aadfbb55de9ee0
Yannick Couzinié <yannick.couzinie@cognostics.de>
2017-09-30 11:23:44 +0200

    Small fixes and new dictionary

--- add_ne_tag.py
+++ add_ne_tag.py
@@ -1,8 +1,8 @@
 """
 Short module that should be run in case you didn't run disambiguate.py
 with NER on but still want to use the NER tags. It will use all entries
-that begin with an it and replace the it with <NE> and then write the new
-dictionary disambiguations_with_ner.yaml.
+that begin with he, she or it and replace the it with <NE> and then write
+the new dictionary disambiguations_with_ner.yaml.
 
 Note: This will delete the original yaml file.
 """
@@ -11,11 +11,12 @@ Note: This will delete the original yaml file.
 import yaml
 
 with open("disambiguations.yaml", "r") as stream:
-    inp_dic = yaml.load(stream)
+    INP_DICT = yaml.load(stream)
 
-out_dict = inp_dic.copy()
-for key, value in inp_dic.items():
-    if key[0][0] == 'it':
+OUT_DICT = INP_DICT.copy()
+for key, value in INP_DICT.items():
+    if key[0][0] == 'he' or key[0][0] == 'she' or key[0][0] == 'it':
+        preposition = key[0][0]
         # this is a bad work-around to get the tuple to actually be a
         # single element as a tuple and not automatically converted to a
         # list.
@@ -24,8 +25,8 @@ for key, value in inp_dic.items():
         new_key += key[1:]
         new_key = tuple(new_key)
 
-        out_dict[new_key] = {subkey.replace('it', '<NE>'): subvalue
+        OUT_DICT[new_key] = {subkey.replace(preposition, '<NE>'): subvalue
                              for subkey, subvalue in value.items()}
 
 with open("disambiguations.yaml", "w") as stream:
-    yaml.dump(out_dict, stream)
+    yaml.dump(OUT_DICT, stream)
--- disambiguate.py
+++ disambiguate.py
@@ -269,13 +269,12 @@ if __name__ == '__main__':
     # split up into a list of words
     SENT_LST = nltk.corpus.brown.sents()
     SENT_LST += nltk.corpus.gutenberg.sents()
-    SENT_LST += nltk.corpus.webtext.sents()
     SENT_LST += nltk.corpus.reuters.sents()
     SENT_LST += nltk.corpus.inaugural.sents()
     POS_MODEL = utils.load_stanford('pos')
     NER_MODEL = utils.load_stanford('ner')
     write_dictionary(POS_MODEL,
                      SENT_LST,
-                     add_tags=2,
+                     add_tags=1,
                      use_ner=False,
                      ner_args=[NER_MODEL, "<NE>"])
